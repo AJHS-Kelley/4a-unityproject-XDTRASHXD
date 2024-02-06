ee9b68d871b8cae84b75288018308583a2213fdb
Julian g <90346616+XDTRASHXD@users.noreply.github.com>
2024-02-06 12:02:13 -0800

    .2

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int myInt = 0;
+        float myFloat = 3.14F;
+        bool myBool = true;
+        string myString = "The FSCJ trip was boring.\n";
+
+        Console.WriteLine("My Integer Variable is " + myInt);
+        Console.WriteLine("My Float Variable is " + myFloat);
+        Console.WriteLine("My Bool Variable is " + myBool);
+        Console.WriteLine("My String Variable is " + myString);
     }
 }
